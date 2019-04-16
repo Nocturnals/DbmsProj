fcc5446cc2ee0d805b58343ac63255fa77fc67a8
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-04-17 01:03:02 +0530

    Added verfication for the firebase user and willpopscope

--- songs_app/lib/app screens/login.dart
+++ songs_app/lib/app screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:songs_app/app screens/home.dart';
 import 'package:songs_app/app screens/register.dart';
@@ -21,149 +22,179 @@ class _LoginPageState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: null,
-        title: Text(
-          'BLINK',
-          style: TextStyle(
-            fontSize: 25,
-            fontFamily: 'Velhos Tempos',
+    return WillPopScope(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: null,
+          title: Text(
+            'BLINK',
+            style: TextStyle(
+              fontSize: 25,
+              fontFamily: 'Velhos Tempos',
+            ),
+            textAlign: TextAlign.center,
           ),
-          textAlign: TextAlign.center,
+          centerTitle: true,
+          backgroundColor: Colors.deepPurple,
         ),
-        centerTitle: true,
-        backgroundColor: Colors.deepPurple,
-      ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          children: <Widget>[
-            Container(
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(top: 70),
-                    child: Icon(
-                      Icons.account_circle,
-                      size: 120,
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 10, bottom: 20),
-                    child: Text(
-                      'LOG IN',
-                      style: TextStyle(
-                          fontSize: 40,
-                          fontFamily: 'TooneyNoodle',
-                          color: Colors.deepPurple),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Container(
-                    width: 350,
-                    height: 50,
-                    margin: EdgeInsets.only(bottom: 30),
-                    child: TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      textAlign: TextAlign.justify,
-                      cursorRadius: Radius.circular(5),
-                      cursorColor: Colors.grey,
-                      autocorrect: true,
-                      keyboardAppearance: Brightness.dark,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.deepPurple)),
-                        labelText: 'Email',
-                        hintText: 'example@example.com',
+        body: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 70),
+                      child: Icon(
+                        Icons.account_circle,
+                        size: 120,
                       ),
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return 'Email can\'t be empty';
-                        }
-                        if (!value.contains('@')) {
-                          return 'Please provide correct emailid';
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        _email = value;
-                      },
                     ),
-                  ),
-                  Container(
-                    width: 350,
-                    height: 50,
-                    margin: EdgeInsets.all(5),
-                    child: TextFormField(
-                      textAlign: TextAlign.justify,
-                      cursorRadius: Radius.circular(5),
-                      cursorColor: Colors.grey,
-                      keyboardAppearance: Brightness.dark,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.deepPurple)),
-                        labelText: 'Password',
-                        hintText: '*************',
+                    Container(
+                      margin: EdgeInsets.only(top: 10, bottom: 20),
+                      child: Text(
+                        'LOG IN',
+                        style: TextStyle(
+                            fontSize: 40,
+                            fontFamily: 'TooneyNoodle',
+                            color: Colors.deepPurple),
+                        textAlign: TextAlign.center,
                       ),
-                      obscureText: true,
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return 'Password can\'t be emply';
-                        }
-                        if (value.length < 6) {
-                          return 'Password must be atleast 6 characters';
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        _password = value;
-                      },
                     ),
-                  ),
-                  Container(
+                    Container(
                       width: 350,
-                      margin: EdgeInsets.only(top: 30, bottom: 40),
-                      child: RaisedButton(
-                        child: Text(
-                          'LOG IN',
-                          style: TextStyle(color: Colors.white),
+                      height: 50,
+                      margin: EdgeInsets.only(bottom: 30),
+                      child: TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        textAlign: TextAlign.justify,
+                        cursorRadius: Radius.circular(5),
+                        cursorColor: Colors.grey,
+                        autocorrect: true,
+                        keyboardAppearance: Brightness.dark,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.deepPurple)),
+                          labelText: 'Email',
+                          hintText: 'example@example.com',
                         ),
-                        onPressed: () {
-                          _validateAndSubmit();
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Email can\'t be empty';
+                          }
+                          if (!value.contains('@')) {
+                            return 'Please provide correct emailid';
+                          }
+                          return null;
                         },
-                        color: Colors.indigo,
-                      )),
-                  Container(
-                      child: Column(
-                    children: <Widget>[
-                      Text(
-                        'Not yet registered? tap the register button',
+                        onSaved: (value) {
+                          _email = value;
+                        },
                       ),
-                      Container(
-                        width: 120,
-                        child: FlatButton(
+                    ),
+                    Container(
+                      width: 350,
+                      height: 50,
+                      margin: EdgeInsets.all(5),
+                      child: TextFormField(
+                        textAlign: TextAlign.justify,
+                        cursorRadius: Radius.circular(5),
+                        cursorColor: Colors.grey,
+                        keyboardAppearance: Brightness.dark,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.deepPurple)),
+                          labelText: 'Password',
+                          hintText: '*************',
+                        ),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Password can\'t be emply';
+                          }
+                          if (value.length < 6) {
+                            return 'Password must be atleast 6 characters';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _password = value;
+                        },
+                      ),
+                    ),
+                    Container(
+                        width: 350,
+                        margin: EdgeInsets.only(top: 30, bottom: 40),
+                        child: RaisedButton(
                           child: Text(
-                            'Register',
-                            style: TextStyle(fontSize: 15),
+                            'LOG IN',
+                            style: TextStyle(color: Colors.white),
                           ),
                           onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => Register()));
+                            _validateAndSubmit();
                           },
-                          textColor: Colors.blue,
+                          color: Colors.indigo,
+                        )),
+                    Container(
+                        child: Column(
+                      children: <Widget>[
+                        Text(
+                          'Not yet registered? tap the register button',
                         ),
-                      )
-                    ],
-                  )),
-                ],
+                        Container(
+                          width: 120,
+                          child: FlatButton(
+                            child: Text(
+                              'Register',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Register()));
+                            },
+                            textColor: Colors.blue,
+                          ),
+                        )
+                      ],
+                    )),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
+      onWillPop: () {
+        AlertDialog alertDialog = AlertDialog(
+          title: Center(
+            child: Text('Are you sure'),
+          ),
+          content: Text('  want to close the app?'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Not Yet'),
+              onPressed: () {
+                Navigator.of(context, rootNavigator: true).pop('dialog');
+              },
+            ),
+            FlatButton(
+              child: Text('Yes'),
+              onPressed: () {
+                // return exit(0);
+                SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+              },
+            ),
+          ],
+        );
+        showDialog(
+            context: context,
+            builder: (_) {
+              return alertDialog;
+            });
+      },
     );
   }
 
@@ -182,15 +213,53 @@ class _LoginPageState extends State<Login> {
       try {
         String userid = await auth.login(_email, _password);
         print(userid);
-        Navigator.push(context, MaterialPageRoute(builder: (context) {
-          return Home();
-        }));
+        bool userStatus = await auth.isEmailVerified();
+        print('user Status:$userStatus');
+        if (userStatus == false) {
+          showDialog(
+              context: context,
+              builder: (_) {
+                return AlertDialog(
+                  title: Center(
+                    child: Text(
+                      'Please verify your email before login',
+                      style: TextStyle(
+                          color: Colors.red, fontFamily: 'Magnificent'),
+                    ),
+                  ),
+                  content: Text('click on resend mail verfication'),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text('Resend mail'),
+                      onPressed: _resendEmailVerification,
+                    ),
+                    FlatButton(
+                      child: Text('OK'),
+                      onPressed: () {
+                        auth.signOut();
+                        Navigator.of(context,rootNavigator: true).pop('dialog');
+                      },
+                    )
+                  ],
+                );
+              });
+        } else {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return Home();
+          }));
+        }
         // _getUserFromDB();
       } catch (error) {
         print('error: $error');
         _showAlertDialog('Error', error.toString());
       }
     }
+  }
+
+  void _resendEmailVerification() async {
+    await auth.sendEmailVerification();
+    await auth.signOut();
+    Navigator.of(context,rootNavigator: true).pop('dialog');
   }
 
   void _showAlertDialog(String title, String message) {
