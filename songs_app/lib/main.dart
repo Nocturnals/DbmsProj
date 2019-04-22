302a6c4dbcebf4ca9fb6fa416493fc8428e763a1
Sriram <neelakantasriram.n17@iiits.in>
2019-04-22 11:42:26 +0530

    added main

--- songs_app/lib/main.dart
+++ songs_app/lib/main.dart
@@ -7,12 +7,8 @@ import 'package:songs_app/app screens/home.dart';
 import 'package:songs_app/app screens/login.dart';
 import 'package:songs_app/app screens/register.dart';
 import 'package:songs_app/app screens/resetPassword.dart';
-<<<<<<< HEAD
 import 'package:songs_app/app screens/testing/test.dart';
 import 'package:songs_app/app screens/now_playing.dart';
-=======
-// import 'package:songs_app/app screens/test.dart';
->>>>>>> a83a1d97d8d4d2616291c9b91b8a32b35fc3de62
 
 void main() {
   runApp(OnlineMusic());
